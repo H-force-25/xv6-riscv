824575cba8954f41acf2ef25f3ccce60c644a502
He <1961184575@qq.com>
2023-11-16 10:41:09 +0800

    lab 3

--- kernel/defs.h
+++ kernel/defs.h
@@ -178,6 +178,18 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+void            vmprint(pagetable_t);
+pagetable_t     ukvminit(void);
+void            ukvmmap(pagetable_t kpagetable, uint64 va, uint64 pa, uint64 sz, int perm);
+void            freeprockvm(struct proc* p);
+int             umappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm);
+int             pagecopy(pagetable_t oldpage, pagetable_t newpage, uint64 begin, uint64 end);
+void            ukvminithard(pagetable_t page);
+
+// vmcopyin.c
+int             copyin_new(pagetable_t, char *, uint64, uint64);
+int             copyinstr_new(pagetable_t, char *, uint64, uint64);
+
 
 // plic.c
 void            plicinit(void);
--- kernel/exec.c
+++ kernel/exec.c
@@ -116,6 +116,15 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
+  if (pagecopy(p->pagetable, p->kpagetable, 0, p->sz) != 0) {
+    goto bad;
+  }
+
+  w_satp(MAKE_SATP(p->kpagetable));
+  sfence_vma();
+
+  if (p->pid == 1)
+    vmprint(p->pagetable);
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
